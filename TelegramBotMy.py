3b71c89098332ab331b0610e0f2b3b82702e5ed2
VorTx828 <Sp060504@gmail.com>
2024-11-29 11:47:05 +0500

    2
    Добавил список подписанных на бота пользователей и метод, проходящийся по этому списку для рассылки картинки каждому пользователю. Также я вынес данный метод в отдельный поток, чтобы сам бот в это время продолжал свою работу. Добавил метод, добавляющий айди авторизованных пользователей в список.

--- TelegramBotMy.py
+++ TelegramBotMy.py
@@ -3,12 +3,28 @@ import threading as th
 
 TOKEN = '8101296963:AAGKmU4KbCmP6jb8tDHPVrtmSsqP6zdUHhc'
 bot = telebot.TeleBot(TOKEN)
+subscribed_users = set()
 
+def Notifications():
+    while True:
+        input()
+        photo_path = 'C:\\Users\\Viacheslav\\Pictures\\1.png'  # Замените на путь к вашему изображению
+        with open(photo_path, 'rb') as photo:
+            for user_id in subscribed_users:
+                try:
+                    bot.send_photo(user_id, photo)
+                except Exception as e:
+                    print(f"Не удалось отправить фото пользователю {user_id}: {e}")
+
+
+tr = th.Thread(target=Notifications)
+tr.start()
 
 
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
+    subscribed_users.add(message.chat.id)
     bot.reply_to(message, "Привет! Я ваш Telegram-бот.")
 
 # Обработчик команды /help
